83e6c6e62e046bd5fee1644b7dd44ef770a16009
Uriel Geraldo <a20490699@itmexicali.edu.mx>
2024-08-12 17:14:26 -0700

    Update: Added blueprint for user settings and __init__.py and routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 # Importing Flask, Configurations and App Extensions
 from flask import Flask
+from app.functions import functions
 from app.extensions import db, migrate
 from app.config import DatabaseConfig, AppConfig
 # Importing Flask, Configurations and App Extensions
@@ -8,11 +9,14 @@ from app.config import DatabaseConfig, AppConfig
 from app.blueprints.home import home_bp
 from app.blueprints.auth import auth_bp
 from app.blueprints.profile import profile_bp
+from app.blueprints.settings import settings_bp
 from app.blueprints.sites import sites_bp
 from app.blueprints.users import users_bp
 from app.blueprints.routers import routers_bp
 from app.blueprints.regions import regions_bp
 from app.blueprints.dashboard import dashboard_bp
+
+
 # Importing Blueprints
 
 # Function constructor to create the app
@@ -29,6 +33,7 @@ def create_app():
     app.register_blueprint(home_bp, url_prefix='/')  # Registering the home blueprint
     app.register_blueprint(auth_bp, url_prefix='/auth')  # Registering the auth blueprint
     app.register_blueprint(profile_bp, url_prefix='/profile')  # Registering the profile blueprint
+    app.register_blueprint(settings_bp, url_prefix='/settings')  # Registering the settings blueprint
     app.register_blueprint(sites_bp, url_prefix='/sites')  # Registering the sites blueprint
     app.register_blueprint(users_bp, url_prefix='/users')  # Registering the users blueprint
     app.register_blueprint(routers_bp, url_prefix='/routers')  # Registering the routers blueprint
