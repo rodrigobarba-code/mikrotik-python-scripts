92cfbc920123c93b749dcf9f08aaa1c8a4ee2026
Uriel Geraldo <a20490699@itmexicali.edu.mx>
2024-08-26 15:37:35 -0700

    Update: fixed local ip method

--- app/functions.py
+++ app/functions.py
@@ -1,6 +1,8 @@
 # Importing Necessary Libraries
-import socket
+import socket, platform
 import requests as r
+
+
 # Importing Necessary Libraries
 
 # Get Public IP Address Function
@@ -13,11 +15,26 @@ def get_public_ip():
 
     data = response.json()  # Get the JSON data from the response
     return data['ip']  # Return the public IP address
+
+
 # Get Public IP Address Function
+
 
 # Get Local IP Address Function
 def get_local_ip():
-    return socket.gethostbyname(socket.gethostname())  # Return the local IP address
+    try:
+        # Create a socket and connect to an external IP address
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        local_ip = s.getsockname()[0]
+        s.close()
+        return local_ip  # Return the local IP address
+    except Exception as e:
+        print(f"Error: {e}")
+        return None  # Return None if there is an error
+# Get Local IP Address Function
+
+
 # Get Local IP Address Function
 
 # Create object for global functions
