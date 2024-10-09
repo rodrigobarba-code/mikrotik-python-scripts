4bf5720905543daf1695b9ac61b329cf41b321e5
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-10-09 14:09:11 -0700

    SCRUM-24 fix(scan): repair router scan only on that section, in-section scanning

--- app/static/js/router_scan/scripts.js
+++ app/static/js/router_scan/scripts.js
@@ -1,19 +1,29 @@
 let socket = io();
 
 document.getElementById('start-btn').addEventListener('click', function() {
-    document.getElementById('pb-container').removeAttribute('hidden');
+    Swal.fire({
+        icon: 'info',
+        title: 'Scanning router...',
+        text: 'Please wait while we scan the routers...',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showConfirmButton: false,
+        didOpen: () => {
+            Swal.showLoading();
+        }
+    });
 
     socket.emit('start_scan');
 });
 
 socket.on('router_scan_finished', function() {
-    document.getElementById('pb-container').setAttribute('hidden', 'true');
     Swal.fire({
         icon: 'success',
         title: 'Router scan finished!',
         text: 'The scan has been completed successfully, you can now view the results.',
         confirmButtonText: 'View results',
         showCancelButton: true,
+        allowOutsideClick: false,
         cancelButtonText: 'Close',
         cancelButtonColor: '#d33'
     }).then((result) => {
