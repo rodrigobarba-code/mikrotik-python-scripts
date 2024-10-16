ee133b32642286a5b4691fd6bf7ef928ef88774a
Uriel Geraldo <a20490699@itmexicali.edu.mx>
2024-10-16 16:26:45 -0700

    Revert "SCRUM-28 feat: router for blacklist"
    This reverts commit f6e7dee92482f73d9a68c3bbcf65a7802ae06a47.

--- api/fastapi/__init__.py
+++ api/fastapi/__init__.py
@@ -9,7 +9,6 @@ from .routes.routers_api import routers_router
 # from .routes.arptags_api import arptags_router
 from .routes.segments_api import segments_router
 from .routes.settings_api import settings_router
-from .routes.blacklist_api import blacklist_router
 
 private_router = APIRouter(prefix='/private', tags=['Seven Suite API'])
 
@@ -22,4 +21,3 @@ private_router.include_router(routers_router, tags=['Routers'])
 # private_router.include_router(arptags_router, tags=['ARP Tags'])
 private_router.include_router(segments_router, tags=['Segments'])
 private_router.include_router(settings_router, tags=['Settings'])
-private_router.include_router(blacklist_router, tags=['Blacklist'])
