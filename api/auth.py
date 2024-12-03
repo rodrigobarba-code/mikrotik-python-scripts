74964986789b5cd08f83fdb2990b57498495349d
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-12-03 14:00:57 -0800

    SCRUM-42 fix(jwt)!: comment auth0 authentication via api

--- api/auth.py
+++ api/auth.py
@@ -16,45 +16,57 @@ ALGORITHMS = os.getenv("ALGORITHMS")
 
 security = HTTPBearer()
 
+
 def get_auth0_jwks():
     jwks_url = f"https://{AUTH0_DOMAIN}/.well-known/jwks.json"
     jwks = requests.get(jwks_url).json()
     return jwks
 
+
 def verify_jwt(token: str = Depends(security)):
-    jwks = get_auth0_jwks()
+    """
+    Code snippet from api/auth.py, to Auth0 JWT verification
+    :param token: JWT token
+    :return: payload
+    """
+    """
+        jwks = get_auth0_jwks()
 
-    try:
-        unverified_header = jwt.get_unverified_header(token.credentials)
-        rsa_key = {}
+        try:
+            unverified_header = jwt.get_unverified_header(token.credentials)
+            rsa_key = {}
 
-        for key in jwks["keys"]:
-            if key["kid"] == unverified_header["kid"]:
-                rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
-                }
+            for key in jwks["keys"]:
+                if key["kid"] == unverified_header["kid"]:
+                    rsa_key = {
+                        "kty": key["kty"],
+                        "kid": key["kid"],
+                        "use": key["use"],
+                        "n": key["n"],
+                        "e": key["e"]
+                    }
 
-        if rsa_key:
-            payload = jwt.decode(
-                token.credentials,
-                rsa_key,
-                algorithms=ALGORITHMS,
-                audience=API_AUDIENCE,
-                issuer=f"https://{AUTH0_DOMAIN}/"
+            if rsa_key:
+                payload = jwt.decode(
+                    token.credentials,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer=f"https://{AUTH0_DOMAIN}/"
+                )
+                return payload
+
+            raise HTTPException(
+                status_code=HTTP_403_FORBIDDEN,
+                detail="Could not validate credentials"
             )
-            return payload
 
-        raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials"
-        )
+        except JWTError:
+            raise HTTPException(
+                status_code=HTTP_403_FORBIDDEN,
+                detail="Invalid token"
+            )
+        """
 
-    except JWTError:
-        raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN,
-            detail="Invalid token"
-        )
+    if True:
+        return {"sub": "1234567890", "name": "John Doe"}
