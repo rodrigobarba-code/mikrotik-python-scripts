c79baacc42052a18b257c5e81149f848a3084fe3
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-12-11 17:20:57 -0800

    SCRUM-51 feat(docker)!: change logic on entrypoint for app seven suite image

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 set -e  # If a command fails then the entrypoint stops
 
+# Check if the database is ready
 echo "Waiting for MySQL to be ready..."
 until mysql -h mariadb-database -u sevensuiteuser -pdevelopment-sevensuiteapp -e "SELECT 1" &>/dev/null; do
-  sleep 2
+    echo "MySQL is unavailable - sleeping"
+    sleep 1
 done
 
-echo "Creating the database if it does not exist..."
-mysql -h mariadb-database -u sevensuiteuser -pdevelopment-sevensuiteapp -e "CREATE DATABASE IF NOT EXISTS sevensuite;"
+# Check if the database exists
+echo "Checking if the database exists..."
+# Verify if not give error
+if mysql -h mariadb-database -u sevensuiteuser -pdevelopment-sevensuiteapp -e "USE sevensuite;" 2>/dev/null; then
+    if mysql -h mariadb-database -u sevensuiteuser -pdevelopment-sevensuiteapp -e "USE sevensuite; SHOW TABLES;" | grep -q "alembic_version"; then
+        echo "Database exists and is up to date."
+        echo "Starting the app..."
+        exec python3 run.py
+    else
+        echo "Database exists but is not up to date."
 
-echo "Applying the migrations..."
-alembic revision --autogenerate -m "Rebase migration"
-alembic upgrade head
+        echo "Creating the migration revision..."
+        alembic revision --autogenerate -m "Rebase migration"
 
-echo "Importing the database dump..."
-mysql -h mariadb-database -u sevensuiteuser -pdevelopment-sevensuiteapp sevensuite < api/dump.sql
+        echo "Applying the migrations..."
+        alembic upgrade head
 
-echo "Starting the app..."
-exec python3 run.py
+        echo "Importing the database dump..."
+        mysql -h mariadb-database -u sevensuiteuser -pdevelopment-sevensuiteapp sevensuite < api/dump.sql
+
+        echo "Starting the app..."
+        exec python3 run.py
+    fi
+fi
+
+
+
+
+
+
+
+
